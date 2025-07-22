54291ba0c3e236b69568a8a8dd48539e4190733d
savchenkoaddev <p.savchenkoad.dev@gmail.com>
2025-07-22 17:35:34 +0300

    Add Docker Compose with PostgreSQL

---
+++ src/TagTheSpot.Services.User.Infrastructure/Persistence/Options/DbSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TagTheSpot.Services.User.Infrastructure.Persistence.Options
+{
+    public sealed class DbSettings
+    {
+        public const string SectionName = nameof(DbSettings);
+
+        [Required]
+        public required string ConnectionString { get; init; }
+    }
+}
