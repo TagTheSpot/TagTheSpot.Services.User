3db365d541e05decfa557f4c2a47f3e7f0d52d33
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-07-23 14:03:37 +0300

    [TTS-32] Implement Register Endpoint (#7)
    * Add Validators
    * Implement the endpoint
    
    ---------
    
    https://psavchenkoad.atlassian.net/browse/TTS-32?atlOrigin=eyJpIjoiOTU2OWYwZDQ0ZTVmNDFkNzgwMDJjNjVkYWQyODEzZjQiLCJwIjoiaiJ9

---
+++ src/TagTheSpot.Services.User.Application/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using TagTheSpot.Services.User.Application.DTO;
+
+namespace TagTheSpot.Services.User.Application.Validators
+{
+    public sealed class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+            Include(new EmailValidator<RegisterRequest>(x => x.Email));
+            Include(new PasswordValidator<RegisterRequest>(x => x.Password));
+        }
+    }
+}
---
+++ src/TagTheSpot.Services.User.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TagTheSpot.Services.User.Application.Abstractions.Services;
+using TagTheSpot.Services.User.Application.DTO;
+using TagTheSpot.Services.User.WebAPI.Factories;
+
+namespace TagTheSpot.Services.User.WebAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly ProblemDetailsFactory _problemDetailsFactory;
+
+        public UsersController(
+            IUserService userService, 
+            ProblemDetailsFactory problemDetailsFactory)
+        {
+            _userService = userService;
+            _problemDetailsFactory = problemDetailsFactory;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(
+            [FromBody] RegisterRequest request)
+        {
+            var result = await _userService.RegisterAsync(request);
+
+            return result.IsSuccess ? Ok(result.Value) : _problemDetailsFactory.GetProblemDetails(result);
+        }
+    }
+}
