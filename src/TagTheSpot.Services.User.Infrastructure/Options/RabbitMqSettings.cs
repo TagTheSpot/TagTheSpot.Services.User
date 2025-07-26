f43dcc2d77c7d6fe007301858d2b303ce9d6916c
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-07-26 09:38:18 +0300

    [TTS-50] Add publishing events in User (#11)
    * Add MassTransit Config
    * Migrate to RabbitMQ
    
    ---------
    
    https://psavchenkoad.atlassian.net/browse/TTS-50?atlOrigin=eyJpIjoiMmM3ZGY0YjM1YWQwNGRmOThmZTVjZTI3NThlYWM5MDQiLCJwIjoiaiJ9

---
+++ src/TagTheSpot.Services.User.Infrastructure/Options/RabbitMqSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TagTheSpot.Services.User.Infrastructure.Options
+{
+    public sealed class RabbitMqSettings
+    {
+        public const string SectionName = nameof(RabbitMqSettings);
+
+        [Required]
+        public required string Host { get; init; }
+
+        [Required]
+        public required string VirtualHost { get; init; }
+
+        [Required]
+        public required string Username { get; init; }
+
+        [Required]
+        public required string Password { get; init; }
+    }
+}
--- src/TagTheSpot.Services.User.WebAPI/Program.cs
+++ src/TagTheSpot.Services.User.WebAPI/Program.cs
@@ -1,23 +1,25 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MassTransit;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using TagTheSpot.Services.User.Application.Abstractions.Identity;
 using TagTheSpot.Services.User.Application.Abstractions.Services;
 using TagTheSpot.Services.User.Application.Identity;
+using TagTheSpot.Services.User.Application.Options;
 using TagTheSpot.Services.User.Application.Services;
+using TagTheSpot.Services.User.Application.Validators;
 using TagTheSpot.Services.User.Infrastructure.Authentication.Options;
+using TagTheSpot.Services.User.Infrastructure.Extensions;
+using TagTheSpot.Services.User.Infrastructure.Options;
 using TagTheSpot.Services.User.Infrastructure.Persistence;
 using TagTheSpot.Services.User.Infrastructure.Persistence.Options;
 using TagTheSpot.Services.User.Infrastructure.Services;
+using TagTheSpot.Services.User.WebAPI.Extensions;
 using TagTheSpot.Services.User.WebAPI.Factories;
 using TagTheSpot.Services.User.WebAPI.Middleware;
-using TagTheSpot.Services.User.Infrastructure.Extensions;
-using FluentValidation.AspNetCore;
-using FluentValidation;
-using TagTheSpot.Services.User.Application.Validators;
-using TagTheSpot.Services.User.Application.Options;
 using TagTheSpot.Services.User.WebAPI.Options;
-using TagTheSpot.Services.User.WebAPI.Extensions;
 
 namespace TagTheSpot.Services.User.WebAPI
 {
@@ -48,6 +50,11 @@ namespace TagTheSpot.Services.User.WebAPI
             .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultTokenProviders();
 
+            builder.Services.AddOptions<RabbitMqSettings>()
+                .BindConfiguration(RabbitMqSettings.SectionName)
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
             builder.Services.AddOptions<JwtSettings>()
                 .BindConfiguration(JwtSettings.SectionName)
                 .ValidateDataAnnotations()
@@ -72,6 +79,23 @@ namespace TagTheSpot.Services.User.WebAPI
 
             builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<ITokenService, JwtTokenService>();
+
+            builder.Services.AddMassTransit(busConfigurator =>
+            {
+                busConfigurator.SetKebabCaseEndpointNameFormatter();
+
+                busConfigurator.UsingRabbitMq((context, configurator) =>
+                {
+                    RabbitMqSettings settings = context
+                        .GetRequiredService<IOptions<RabbitMqSettings>>().Value;
+
+                    configurator.Host(settings.Host, settings.VirtualHost, h =>
+                    {
+                        h.Username(settings.Username);
+                        h.Password(settings.Password);
+                    });
+                });
+            });
 
             builder.Services.AddFluentValidationAutoValidation();
             builder.Services.AddValidatorsFromAssemblyContaining<RegisterRequestValidator>();
