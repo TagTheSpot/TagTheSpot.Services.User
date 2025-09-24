553c7b76068b9058c58854b2be7926529ba35b7d
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-09-24 16:57:39 +0300

    [TTS-188] Fix Confirmation Link Generation (#19)
    https://psavchenkoad.atlassian.net/browse/TTS-188?atlOrigin=eyJpIjoiMDFhYzM0MGJmYmE2NDYxMThlZTc2YWIwYzA0MmNiMGYiLCJwIjoiaiJ9

---
+++ src/TagTheSpot.Services.User.Application/Options/EmailLinkGenerationSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TagTheSpot.Services.User.Application.Options
+{
+    public sealed class EmailLinkGenerationSettings
+    {
+        public const string SectionName = nameof(EmailLinkGenerationSettings);
+
+        [Required]
+        [Url]
+        public required string ClientConfirmationLink { get; init; }
+    }
+}
