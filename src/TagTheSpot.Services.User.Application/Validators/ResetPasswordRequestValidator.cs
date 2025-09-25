ccd067d9cd95c452badde348ce78d998ec5795fe
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-09-25 10:38:13 +0300

    [TTS-190] Implement Reset Password Endpoint (#21)
    https://psavchenkoad.atlassian.net/browse/TTS-190?atlOrigin=eyJpIjoiN2U0ZjBhMmM2ZjNmNDY3NWE4ZWEyZDE2ZjAzYzk0MGUiLCJwIjoiaiJ9

---
+++ src/TagTheSpot.Services.User.Application/Validators/ResetPasswordRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TagTheSpot.Services.User.Application.DTO;
+
+namespace TagTheSpot.Services.User.Application.Validators
+{
+    public sealed class ResetPasswordRequestValidator
+        : AbstractValidator<ResetPasswordRequest>
+    {
+        public ResetPasswordRequestValidator()
+        {
+            Include(new EmailValidator<ResetPasswordRequest>(x => x.Email));
+
+            Include(new PasswordValidator<ResetPasswordRequest>(x => x.NewPassword));
+
+            RuleFor(x => x.Token)
+                .NotEmpty();
+        }
+    }
+}
